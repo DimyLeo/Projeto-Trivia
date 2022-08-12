168cc465c5e62fd6b69aba931a911e9904b7999b
DimyLeo <DimyLeo@users.noreply.github.com>
2022-08-12 14:07:47 +0000

    deploy: 47bae53e9d7c3d3e9d9e1482e78c16f45d7c0f8f

---
+++ precache-manifest.1408c85770fd8332ec346b05c86eb888.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791fee7b78996e4ff91a07584fc8181a",
+    "url": "/Projeto-Trivia/index.html"
+  },
+  {
+    "revision": "641464929571753c630f",
+    "url": "/Projeto-Trivia/static/css/main.3c4c5fe0.chunk.css"
+  },
+  {
+    "revision": "965e027114f484e7a1ed",
+    "url": "/Projeto-Trivia/static/js/2.0c1e5f96.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/Projeto-Trivia/static/js/2.0c1e5f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641464929571753c630f",
+    "url": "/Projeto-Trivia/static/js/main.e73bdfc7.chunk.js"
+  },
+  {
+    "revision": "4b67b7bfde68c8d303b2",
+    "url": "/Projeto-Trivia/static/js/runtime-main.b0b71741.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Projeto-Trivia/static/media/trivia.466d153e.png"
+  }
+]);
